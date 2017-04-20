5dd99f3482f103f7a3ac5ef83a07a36d15bbe17d
Carlos Nascimento <carlos.nascimento@adtsys.com.br>
2017-04-20 17:59:37 -0300

    Remove frontpage layout

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule SchoolAgenda.PageController do
   use SchoolAgenda.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", layout: {SchoolAgenda.LayoutView, "welcome.html"}
   end
 end
--- web/controllers/plugs/auth_plug.ex
+++ web/controllers/plugs/auth_plug.ex
@@ -1,12 +1,20 @@
 defmodule SchoolAgenda.Plugs.AuthPlug do
   import Plug.Conn
+  import Phoenix.Controller
+
   def init(options), do: options
 
   def call(conn, _options) do
     id = conn |> get_session(:user_id)
-    if id do
-      
+    if !id do
+      do_redirect(conn, "/login")
     end
     conn
   end
+
+  defp do_redirect(conn, to) do
+   conn
+     |> redirect(to: to)
+     |> halt
+ end
 end
